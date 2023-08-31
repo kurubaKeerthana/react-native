71cddec05c55fed7840c8176253728ed8d56af8c
kurubaKeerthana <141502204+kurubaKeerthana@users.noreply.github.com>
2023-08-31 10:05:58 +0530

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@
  * https://github.com/facebook/react-native
  *
  * @format
- */
+ 
 
 import React,{Component} from 'react';
 import {Platform,StyleSheet,Text,View} from 'react-native';
@@ -35,4 +35,28 @@ import {Platform,StyleSheet,Text,View} from 'react-native';
       fontStyle:'italic'
     }
   }
- );
+ );*/
+ import React, {Component} from 'react';  
+import { StyleSheet,Text, View } from 'react-native';  
+  
+export default class App extends Component {  
+    render(){
+      return(
+        <View style={Styles.container}>
+         <View style={{backgroundColor:'blue',flex:0.3}}/>
+         <View style={{backgroundColor:'red',flex:0.5}}/>
+         <Text style={{fontSize:18}}>View Example</Text>
+        </View>
+      );
+        
+    }
+}  
+const Styles=StyleSheet.create({
+  container:{
+    flex:1,
+    flexDirection:'row',
+    height:100,
+    width:"80%",
+    backgroundColor:"yellow"
+  }
+})
