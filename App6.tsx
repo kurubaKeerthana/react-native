0b17f14cf8a8fed5399d1476a67156d7046819bf
kurubaKeerthana <141502204+kurubaKeerthana@users.noreply.github.com>
2023-09-05 08:31:42 +0530

    Add files via upload

---
+++ App6.tsx
@@ -0,0 +1,21 @@
+import React,{Component} from 'react';
+import { ScrollView,Text } from 'react-native';
+export default class App extends Component{
+  render(){
+    return(
+      <ScrollView>
+        <Text style={{fontSize:100,color:'red'}}>hi</Text>
+        <Text style={{fontSize:100,color:'powderblue'}}>hii</Text>
+        <Text style={{fontSize:100,color:'yellow'}}>hiii</Text>
+        <Text style={{fontSize:100,color:'green'}}>hiiii</Text>
+        <Text style={{fontSize:100,color:'skyblue'}}>hiiiii</Text>
+        <Text style={{fontSize:100,color:'black'}}>hiiiiii</Text>
+        <Text style={{fontSize:100,color:'pink'}}>hiiiiiii</Text>
+        <Text style={{fontSize:100,color:'orange'}}>hiiiiiiiii</Text>
+        <Text style={{fontSize:100,color:'brown'}}>hiiiiiiiiii</Text>
+        <Text style={{fontSize:100,color:'steelblue'}}>hiiiiiiiiiii</Text>
+        <Text style={{fontSize:100,color:'blue'}}>hiiiiiiiiiiii</Text>
+      </ScrollView>
+    )
+  }
+}
